db75d7765436125743a0ee22b06bdafe9e070610
silenusproject-lab <silenus.project@gmail.com>
2025-10-15 14:18:00 +0900

    Initial Commit

---
+++ Kamuro/KamuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  KamuroApp.swift
+//  Kamuro
+//
+//  Created by tsuyoshi nishigaki on 2025/10/15.
+//
+
+import SwiftUI
+
+@main
+struct KamuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
